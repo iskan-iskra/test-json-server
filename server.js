c31ff37840fc4dc87a236cfaa0e191c04fdd11e2
iskan-iskra <bf2iskan@gmail.com>
2024-06-02 03:40:21 +0500

    test

--- server.js
+++ server.js
@@ -1,288 +1,45 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
-// Создайте приложение express
+const prisma = new PrismaClient();
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Enable CORS for all routes
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-
-// Подключитесь к MongoDB Atlas
-const dbURI =
-  "mongodb+srv://test:test@cluster0.wzjsmhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-// Создайте схему и модель для задачи
-const todoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  dueDate: Date,
-  createdDate: { type: Date, default: Date.now },
-  completed: { type: Boolean, default: false },
+// Обработчик для получения списка задач
+app.get("/api/todos", async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  res.json(todos);
 });
 
-const Todo = mongoose.model("Todo", todoSchema);
-
-// Swagger setup
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Todo API",
-      version: "1.0.0",
-      description: "API for managing a todo list",
+// Обработчик для создания новой задачи
+app.post("/api/todos", async (req, res) => {
+  const { title, description, completed, dueDate } = req.body;
+  const todo = await prisma.todo.create({
+    data: {
+      title,
+      description,
+      completed,
+      dueDate,
     },
-    // servers: [
-    //   {
-    //     url: `https://localhost:${PORT}`,
-    //   },
-    // ],
-  },
-  apis: ["./server.js"],
-};
-
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.1/swagger-ui.min.css";
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use(
-  "/swagger",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL })
-);
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Todo:
- *       type: object
- *       required:
- *         - title
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the todo
- *         title:
- *           type: string
- *           description: The title of the todo
- *         description:
- *           type: string
- *           description: The description of the todo
- *         dueDate:
- *           type: string
- *           format: date
- *           description: The due date of the todo
- *         createdDate:
- *           type: string
- *           format: date
- *           description: The date the todo was created
- *         completed:
- *           type: boolean
- *           description: The status of the todo
- */
-
-/**
- * @swagger
- * tags:
- *   name: Todos
- *   description: The todo managing API
- */
-
-/**
- * @swagger
- * /todos:
- *   get:
- *     summary: Returns the list of all the todos
- *     tags: [Todos]
- *     responses:
- *       200:
- *         description: The list of the todos
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Todo'
- */
-app.get("/todos", async (req, res) => {
-  try {
-    const todos = await Todo.find();
-    res.json(todos);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-/**
- * @swagger
- * /todos/{id}:
- *   get:
- *     summary: Get the todo by id
- *     tags: [Todos]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The todo id
- *     responses:
- *       200:
- *         description: The todo description by id
- *         contents:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Todo'
- *       404:
- *         description: The todo was not found
- */
-app.get("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
-    if (todo) {
-      res.json(todo);
-    } else {
-      res.status(404).json({ message: "Todo not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-/**
- * @swagger
- * /todos:
- *   post:
- *     summary: Create a new todo
- *     tags: [Todos]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Todo'
- *     responses:
- *       201:
- *         description: The todo was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Todo'
- *       500:
- *         description: Some server error
- */
-app.post("/todos", async (req, res) => {
-  const todo = new Todo({
-    title: req.body.title,
-    description: req.body.description,
-    dueDate: req.body.dueDate,
   });
-
-  try {
-    const newTodo = await todo.save();
-    res.status(201).json(newTodo);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+  res.json(todo);
 });
 
-/**
- * @swagger
- * /todos/{id}:
- *   put:
- *     summary: Update the todo by the id
- *     tags: [Todos]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The todo id
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Todo'
- *     responses:
- *       200:
- *         description: The todo was updated
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Todo'
- *       404:
- *         description: The todo was not found
- *       500:
- *         description: Some error happened
- */
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
-    if (todo) {
-      todo.title = req.body.title;
-      todo.description = req.body.description;
-      todo.dueDate = req.body.dueDate;
-      todo.completed = req.body.completed;
-
-      const updatedTodo = await todo.save();
-      res.json(updatedTodo);
-    } else {
-      res.status(404).json({ message: "Todo not found" });
-    }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+// Обработчик для удаления задачи
+app.delete("/api/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const todo = await prisma.todo.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(todo);
 });
 
-/**
- * @swagger
- * /todos/{id}:
- *   delete:
- *     summary: Remove the todo by id
- *     tags: [Todos]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The todo id
- *     responses:
- *       204:
- *         description: The todo was deleted
- *       404:
- *         description: The todo was not found
- */
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
-    if (todo) {
-      await todo.remove();
-      res.status(204).send();
-    } else {
-      res.status(404).json({ message: "Todo not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
